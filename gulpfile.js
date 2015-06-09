1e34003f0b5fbd7503064c527be5dea74cb7aaef
quanwang <quan.wang@ele.me>
2015-06-09 18:01:26 +0800

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-ruby-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    bower = require('bower'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-ruby-sass'),
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    sh = require('shelljs'),
+    browserify = require('browserify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    uglify = require('gulp-uglify'),
+    replaceHtml = require('gulp-html-replace');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
+
+var liveReload = false;
+var serverPort = 8888;
+
+gulp.task('clean', function () {
+    return gulp.src(
+        [
+            './www/'
+        ],
+        {
+            read: false
+        }
+    ).pipe(
+        clean()
+    );
+});
 
 gulp.task('default', ['sass']);
 
@@ -28,6 +50,103 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('browserify:dev', function () {
+    browserify(
+        './src/js/app.js'
+    ).bundle(
+        {debug: true}
+    ).on(
+        'error',
+        function (err) {
+            console.log(err.message);
+        }
+    ).pipe(
+        source('app.bundle.js')
+    ).pipe(
+        buffer()
+    ).pipe(
+        sourcemaps.init({loadMaps: true})
+    ).pipe(
+        sourcemaps.write('./')
+    ).pipe(
+        gulp.dest('./src/js/')
+    );
+});
+
+gulp.task('browserify:dist', function () {
+    browserify(
+        './src/js/app.js'
+    ).bundle(
+        {debug: true}
+    ).pipe(
+        source('app.min.js')
+    ).pipe(
+        buffer()
+    ).pipe(
+        uglify()
+    ).pipe(
+        gulp.dest('./www/js/')
+    );
+});
+
+gulp.task('copy', function () {
+    gulp.src(
+        './src/img/**/*'
+    ).pipe(
+        gulp.dest('./www/img/')
+    );
+
+    gulp.src([
+        './src/lib/ionic/js/ionic.bundle.min.js'
+    ]).pipe(
+        gulp.dest('./www/js/')
+    );
+});
+
+gulp.task('server', function () {
+    connect.server({
+        livereload: liveReload,
+        root: 'src',
+        port: serverPort
+    });
+});
+
+gulp.task('open-browser', function () {
+    var options = {
+        url: "http://localhost:" + serverPort
+    };
+    gulp.src(
+        "./src/index.html"
+    ).pipe(
+        open("", options)
+    );
+});
+
+gulp.task('replace-html', function () {
+    gulp.src(
+        './src/index.html'
+    ).pipe(
+        replaceHtml({
+            'css': 'css/app.min.css',
+            'js': [
+                'js/lib.min.js',
+                'cordova.js',
+                'js/app.min.js'
+            ]
+        })
+    ).pipe(
+        gulp.dest('./www/')
+    );
+});
+
+gulp.task('dev', ['sass', 'browserify:dev', 'server'], function () {
+    gulp.start('open-browser');
+});
+
+gulp.task('dist', ['clean'], function () {
+    gulp.start('sass', 'browserify:dist', 'copy', 'replace-html');
 });
 
 gulp.task('install', ['git-check'], function() {
